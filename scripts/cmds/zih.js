177e415362b6ed08e894909274e97454365b9444
Zihad Ahmed <zihad262940@gmail.com>
2025-05-23 22:07:51 +0600

    Update zih.js

--- scripts/cmds/zih.js
+++ scripts/cmds/zih.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-
+const apiUrl = "https://www.noobs-api.top/dipto" 
 module.exports.config ={
     name: "bomber",
     version: "6.9",
-    author: "Zihad", 
+    author: "dipto", 
     countDown: 5,
     role: 2,
     category: "admin",
@@ -17,9 +17,8 @@ try{
 const number = args[0]
 const limit = args[1]
 const ok = message.reply("Please wait.....")
-const { data } = await axios.get(`${global.GoatBot.config.api }/bomber?number=${number}&limit=${limit || 1}&key=dipto00869`)
-message.unsend(o
-               k.messageID)
+const { data } = await axios.get(`${apiUrl}/bomber?number=${number}&limit=${limit || 1}&key=dipto00869`)
+message.unsend(ok.messageID)
 message.reply(data.message)
     } catch (error) {
       console.error(error);
